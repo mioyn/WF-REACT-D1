ce59aefe1ab760714b52eaae7e3f7caf6489b4f6
Midhun Jose <midhun.jose485@gmail.com>
2018-05-02 11:20:29 +0530

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import {button, header} from './fetchselectors'
+
+var togflag = false;
+
+function manageToggle() {
+    if(togflag) {header.style.display = "block";}
+    else { header.style.display = "none"; }
+}
+
+button.addEventListener('click', handleClick)
+
+manageToggle();
+
+function handleClick() {
+    togflag = !togflag;
+    manageToggle();
+}
---
+++ src/js/fetchselectors.js
@@ -0,0 +1,3 @@
+export var button = document.querySelector('#myButton');
+export var header = document.querySelector('h3')
+
